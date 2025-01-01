0e61cbee634ac22209d17ad547ec69e3e4aaa04c
shubham yadav <126192924+yadavshubham01@users.noreply.github.com>
2025-01-01 19:53:39 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import './index.css';
+import HowItWorks from './components/HowItWorks';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 flex items-center justify-center">
+      <HowItWorks />
+    </div>
+  );
+}
+
+export default App;
